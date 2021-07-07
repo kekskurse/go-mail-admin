e020dbe7e03724c3b5e85c7f99800a91cb13a3f8
kekskurse <git@kekskurse.de>
2021-07-07 19:38:09 +0200

    fix systemctl deamon reload

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,6 +1,7 @@
 #/bin/sh
 FILE=/etc/go-mail-admin/go-mail-admin.env
 if [ -f "$FILE" ]; then
+    systemctl daemon-reload
     systemctl restart go-mail-admin
 else
     cp /etc/go-mail-admin/go-mail-admin.env.sample /etc/go-mail-admin/go-mail-admin.env
